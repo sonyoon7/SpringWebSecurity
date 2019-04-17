b3a69cfdb8e07d7a5acfb77d62a1aa4069512b51
EuniceSon <sonyoon7@gmail.com>
2019-04-17 15:43:59 +0900

    aop 설정

---
+++ springSecurity/src/main/java/org/zerock/controller/SecurityController.java
@@ -0,0 +1,18 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+public class SecurityController {
+	
+//	@GetMapping("/security")
+//	public void securityTest() {
+//		log.info("security test...");
+//	}
+//	
+	
+}
